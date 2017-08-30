8b74da1ffc2e8b45606ff6d80b6480980f0b19fa
Liudmyla Prysiazhna <liudaprysiazhna@gmail.com>
2017-08-30 14:39:30 +0300

    Update lab5_1.py

--- lab5_1.py
+++ lab5_1.py
@@ -1,19 +1,19 @@
-
-def clean_list(list_to_clean):
-	list_result=[]
-	list_str=[]
-
-	for item in list_to_clean:
-		
-		for i in list_result:
-			
-			if item not in list_result or str(item) != str(i):
-				list_result.append(item)
-				list_str.append(str(item))
-
-	return list_result
-
 '''
+Розробити функцію clean_list(list_to_clean), 
+яка приймає 1 аргумент -- список будь-яких значень (рядків, цілих та дійсних чисел) довільної довжини, 
+та повертає список, який складається з тих самих значень, але не містить повторів елементів. Це значить, що у випадку наявності значення в початковому списку в кількох екземплярах перший "екземпляр" значення залишається на своєму місці, а другий, третій та ін. видаляються.
+
+Наприклад
+Виклик функції: clean_list([1, 1.0, '1', -1, 1])
+Повертає: [1, 1.0, '1', -1]
+Виклик функції: clean_list(['qwe', 'reg', 'qwe', 'REG'])
+Повертає: ['qwe', 'reg', 'REG']
+Виклик функції: clean_list([32, 32.1, 32.0, -123])
+Повертає: [32, 32.1, 32.0, -123]
+Виклик функції: clean_list([1, 2, 1, 1, 3, 4, 5, 4, 6, '2', 7, 8, 9, 0, 1, 2, 3, 4, 5])
+Повертає: [1, 2, 3, 4, 5, 6, '2', 7, 8, 9, 0]
+'''
+
 def clean_list(list_to_clean):
 	list_result=[]
 	list_str=[]
@@ -24,20 +24,3 @@ def clean_list(list_to_clean):
 			list_str.append(str(item))
 
 	return list_result
-
-'''
-
-
-#l_to=[1, 1.0, '1', -1, 1,1.0]
-l_to=['asd', 'dsa', 1, '1', 1.0]
-#l_to=[]
-#l_to=['qwe', 'reg', 'qwe', 'REG']
-
-print(clean_list(l_to))
-
-
-
-
-
-
-
